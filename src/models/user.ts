c4740b4af96ad5ff92cb4a4e614b7b862a047698
AlejandroDalzotto <aledalzotto15@gmail.com>
2024-03-13 18:32:53 -0300

    add basic user entity

---
+++ src/models/user.ts
@@ -0,0 +1,26 @@
+import { DataTypes } from 'sequelize'
+import database from '../database/connection'
+
+export const User = database.define('User', {
+  name: {
+    type: DataTypes.STRING({ length: 18 }),
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING({ length: 50 }),
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING(),
+    allowNull: false
+  },
+  active: {
+    type: DataTypes.BOOLEAN(),
+    defaultValue: true,
+    allowNull: false
+  }
+}, {
+  timestamps: false,
+  tableName: 'users'
+})
